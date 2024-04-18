947a9ea42ede8816a12c6f5611353206b7e0dfea
abhik199 <abhishekkirar2004@gmail.com>
2024-04-18 23:44:17 +0530

    remove dist

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import { router } from "./routes/root.routes";
 import { prisma } from "./config/config";
 const app = express();
@@ -6,8 +7,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", router);
+app.use("*", function (req, res) {
+  res.render("notFound");
+});
 
 const port = process.env.PORT || 6300;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 async function startServer() {
   try {
     await prisma.$connect();
